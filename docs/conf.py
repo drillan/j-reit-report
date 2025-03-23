a5dde76b9329ea3d1e6c71f536cf46abaa02fcaa
driller <eleshis@gmail.com>
2025-03-23 18:10:50 +0900

    first commit

---
+++ docs/conf.py
@@ -0,0 +1,44 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "昨今の経済情勢からみたJ-REITの優位性検証"
+copyright = "2025, driller"
+author = "driller"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["myst_parser"]
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "furo"
+html_static_path = ["_static"]
+
+
+myst_enable_extensions = [
+    "amsmath",
+    "attrs_inline",
+    "colon_fence",
+    "deflist",
+    "dollarmath",
+    "fieldlist",
+    "html_admonition",
+    "html_image",
+    "linkify",
+    "replacements",
+    "smartquotes",
+    "strikethrough",
+    "substitution",
+    "tasklist",
+]
---
+++ docs/index.md
@@ -0,0 +1,21 @@
+% Gold report documentation master file, created by
+% sphinx-quickstart on Sat Mar 15 10:20:24 2025.
+% You can adapt this file completely to your liking, but it should at least
+% contain the root `toctree` directive.
+
+# 昨今の経済情勢からみたJ-REITの優位性検証
+
+このドキュメントでは、昨今の経済情勢からみたJ-REITの優位性を検証します。2023年後半から2024年にかけての日本経済の特徴を踏まえ、J-REITの投資機会とリスクを包括的に分析しています。
+
+```{toctree}
+:caption: '目次'
+:maxdepth: 2
+
+economic_overview
+jreit_advantages
+sector_analysis
+investor_analysis
+risk_analysis
+quantitative_analysis
+conclusion
+```
